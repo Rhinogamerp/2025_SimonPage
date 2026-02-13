4d7b0f9b5b35ef50139b52886f1dea22287b4494
Rhinogamerp <simonpage0404@gmail.com>
2026-02-13 06:38:33 +0000

    changed Worksheet3.cpp to hello.cpp

--- Exercises/Worksheet3.cpp
+++
@@ -1,8 +0,0 @@
-// begin - Worksheet3.cpp===========
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-    std::cout << "Hello Planet Earth!" << std::endl;
-    return 0;
-}
-// end - Worksheet3.cpp
